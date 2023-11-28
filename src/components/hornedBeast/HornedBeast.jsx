c3e94c03bde694508f1a8ed275caf01f19b2e0c2
zachary moore <zach.moore0628@gmail.com>
2023-11-27 19:15:49 -0600

    gallery built

--- src/components/hornedBeast/HornedBeast.jsx
+++ src/components/hornedBeast/HornedBeast.jsx
@@ -4,6 +4,10 @@ function HornedBeast(props) {
     return (
         <li>   
             <h2>{props.title}</h2>
+            <img src={props.imageUrl} alt={props.title} />
+            <p>{props.description}</p>
         </li>
     )
 }
+
+export default HornedBeast;
